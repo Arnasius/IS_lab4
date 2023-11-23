1c1d0170bdacb176ef3334686c548383ceceb0ec
Arnas Gabijūnas <arnas@8devices.com>
2023-11-23 19:44:22 +0200

    add lab4 files

---
+++ vaizdo_atpazinimas.m
@@ -0,0 +1,105 @@
+close all
+clear all
+clc
+
+pavadinimas = 'train_data7.png';
+pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 10);
+
+P = cell2mat(pozymiai_tinklo_mokymui);
+
+T = [eye(3), eye(3), eye(3), eye(3), eye(3), eye(3), eye(3), eye(3), eye(3), eye(3)];
+% newrb(input, output, error goal, spread, neurons)
+tinklas = newrb(P,T,0,1,3);
+%  Radial basis networks
+P2 = P(:,12:22);
+Y2 = sim(tinklas, P2);
+[a2, b2] = max(Y2);
+
+raidziu_sk = size(P2,2);
+
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+% show the result in command window
+disp(atsakymas)
+% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
+%% þodþio "KADA" poþymiø iðskyrimas 
+%% Extract features of the test image
+pavadinimas = 'bca.jpg';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+%% Raidþiø atpaþinimas
+%% Perform letter/symbol recognition
+% poþymiai ið celiø masyvo perkeliami á matricà
+% features from cell-variable are stored to matrix-variable
+P2 = cell2mat(pozymiai_patikrai);
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% estimating neuran network output for newly estimated features
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+% searching which output gives maximum value
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas | Visualization of result
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+% calculating number of symbols - number of columns
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+    end
+end
+disp(atsakymas)
+
+% pateikime rezultatà komandiniame lange
+% disp(atsakymas)
+% figure(8), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+%% þodþio "FIKCIJA" poþymiø iðskyrimas 
+%% extract features for next/another test image
+pavadinimas = 'aab.jpg';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+%% Raidþiø atpaþinimas
+% poþymiai ið celiø masyvo perkeliami á matricà
+P2 = cell2mat(pozymiai_patikrai);
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+    end
+end
+disp(atsakymas)
+
+% pateikime rezultatà komandiniame lange
+% disp(atsakymas)
+% figure(9), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+
